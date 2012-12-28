52db73c8f02856b8af419d5471540c3ec24ac47f
Sergio Terrasi <sergio.terrasi@gmail.com>
2012-12-28 19:11:58 +0100

    Spinner durante il caricamento.

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ Trasforma un tweet in un banner anni '90.
 bannerTweet is an open source project dual licensed by MIT and GPLv2.
 
 
+## Credits
+* The Daily Egg - http://blog.crazyegg.com/2012/05/08/loading-spinners-css3-animation/
+
 ## Support
 
 * WebSite: http://www.nerdissimo.it/
--- src/assets/js/bannerTweet.js
+++ src/assets/js/bannerTweet.js
@@ -23,14 +23,21 @@ GitHub: https://github.com/Nerdissimo/
 Twitter: @Nerdissimo
 */
 var bannerTweet={
-	globals:{_b:true},
+	globals:{_b:true,semaphore:0},
 	init:function(){
 		this.globals.wrapper=document.getElementById("banners");
+		this.globals.lock=document.getElementById("lock");
+		this.globals.lock.onclick=function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+		};
 		this.globals.tweetsId=location.hash.substring(1,location.hash.length).split(",");
 		for(var i=0;i<this.globals.tweetsId.length;i++) this.get(this.globals.tweetsId[i]);
+		if(!this.globals.semaphore) this.globals.lock.parentNode.removeChild(this.globals.lock);
 	},
 	get:function(p_id) {
 		if(!p_id) return;
+		this.globals.semaphore+=1;
 		var script=document.createElement("script");
 		script.type="text/javascript";
 		script.src="https://api.twitter.com/1/statuses/oembed.json?id="+p_id+"&callback=bannerTweet.show";
@@ -43,6 +50,8 @@ var bannerTweet={
 		if(this.globals._b) this.globals.wrapper.innerHTML=this.globals._b="";
 		window.setTimeout(function(){
 			bannerTweet.globals.wrapper.appendChild(el.getElementsByTagName("blockquote")[0]);
+			bannerTweet.globals.semaphore-=1;
+			if(!bannerTweet.globals.semaphore) bannerTweet.globals.lock.parentNode.removeChild(bannerTweet.globals.lock);
 		},200*Math.random()); // con più banner incrementa la casualità dei movimenti
 	}
 };
